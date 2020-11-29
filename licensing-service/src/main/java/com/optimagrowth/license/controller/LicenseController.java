7172192d6d6fde25f1aaa8ac5fe17c99000a6034
ch <mkmsacc@outlook.com>
2020-11-29 17:46:34 +0700

    Add GET request for licenseId

--- licensing-service/src/main/java/com/optimagrowth/license/controller/LicenseController.java
+++ licensing-service/src/main/java/com/optimagrowth/license/controller/LicenseController.java
@@ -1,10 +1,31 @@
 package com.optimagrowth.license.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.optimagrowth.license.model.License;
+import com.optimagrowth.license.service.LicenseService;
+
 @RestController
-@RequestMapping(value="v1/organization/{organization}/license")
+@RequestMapping(value = "v1/organization/{organizationId}/license")
 public class LicenseController {
 
+	@Autowired
+	private LicenseService licenseService;
+
+	@RequestMapping(value = "/{licenseId}", method = RequestMethod.GET)
+	public ResponseEntity<License> getLicense(
+			@PathVariable("organizationId") String organizationId,
+			@PathVariable("licenseId") String licenseId) {
+
+		License license = licenseService.getLicense(licenseId, organizationId);
+		return ResponseEntity.ok(license);
+	}
 }
