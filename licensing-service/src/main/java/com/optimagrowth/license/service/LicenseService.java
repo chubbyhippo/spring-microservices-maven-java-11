dbd208439c6eccc77d7b8fe8adbf430e68e7fec2
ch <mkmsacc@outlook.com>
2020-12-03 17:47:08 +0700

    Add import UUID

--- licensing-service/src/main/java/com/optimagrowth/license/service/LicenseService.java
+++ licensing-service/src/main/java/com/optimagrowth/license/service/LicenseService.java
@@ -1,3 +1,7 @@
+package com.optimagrowth.license.service;
+
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
