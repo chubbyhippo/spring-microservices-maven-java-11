571a95515fd681f7235c59a12b55801a0ada593a
ch <mkmsacc@outlook.com>
2020-12-03 17:46:02 +0700

    Add import ServiceConfig

--- licensing-service/src/main/java/com/optimagrowth/license/service/LicenseService.java
+++ licensing-service/src/main/java/com/optimagrowth/license/service/LicenseService.java
@@ -2,6 +2,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
+import com.optimagrowth.license.config.ServiceConfig;
 import com.optimagrowth.license.model.License;
 import com.optimagrowth.license.repository.LicenseRepository;
 
