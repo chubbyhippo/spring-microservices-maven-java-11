3054bd1e18f76baee7857c6cf7632807df9680d1
ch <mkmsacc@outlook.com>
2020-12-05 11:40:37 +0700

    Code clean up

--- licensing-service/src/main/java/com/optimagrowth/license/model/utils/ErrorMessage.java
+++ licensing-service/src/main/java/com/optimagrowth/license/model/utils/ErrorMessage.java
@@ -1,23 +1,11 @@
 package com.optimagrowth.license.model.utils;
 
-/**
- * Error message class
- * @author ihuaylupo
- * @version 1.0
- * @since Jun 25, 2018
- */
 public class ErrorMessage {
-	
-    /**
-	 * @param message
-	 * @param code
-	 * @param detail
-	 */
+
 	private String message;
 	private String code;
-    private String detail;
+	private String detail;
 
-    
 	public ErrorMessage(String message, String code, String detail) {
 		super();
 		this.message = message;
@@ -31,53 +19,32 @@ public class ErrorMessage {
 		this.detail = detail;
 	}
 
-    public ErrorMessage(String message) {
-        this(message, "", "");
-    }
+	public ErrorMessage(String message) {
+		this(message, "", "");
+	}
 
-	/**
-	 * @return the message
-	 */
 	public String getMessage() {
 		return message;
 	}
 
-	/**
-	 * @param message the message to set
-	 */
 	public void setMessage(String message) {
 		this.message = message;
 	}
 
-	/**
-	 * @return the code
-	 */
 	public String getCode() {
 		return code;
 	}
 
-	/**
-	 * @param code the code to set
-	 */
 	public void setCode(String code) {
 		this.code = code;
 	}
 
-	/**
-	 * @return the detail
-	 */
 	public String getDetail() {
 		return detail;
 	}
 
-	/**
-	 * @param detail the detail to set
-	 */
 	public void setDetail(String detail) {
 		this.detail = detail;
 	}
-
-    
-
 
 }
--- licensing-service/src/main/java/com/optimagrowth/license/model/utils/RestErrorList.java
+++ licensing-service/src/main/java/com/optimagrowth/license/model/utils/RestErrorList.java
@@ -1,22 +1,12 @@
 package com.optimagrowth.license.model.utils;
 
-
 import org.springframework.http.HttpStatus;
 
 import java.util.ArrayList;
 import static java.util.Arrays.asList;
 
-
-/**
- * Error list
- * @author ihuaylupo
- * @version
- * @since Jun 25, 2018
- */
 public class RestErrorList extends ArrayList<ErrorMessage> {
 
-
-	/** Generated Serial Version*/
 	private static final long serialVersionUID = -721424777198115589L;
 	private HttpStatus status;
 
@@ -30,16 +20,10 @@ public class RestErrorList extends ArrayList<ErrorMessage> {
 		addAll(asList(errors));
 	}
 
-	/**
-	 * @return the status
-	 */
 	public HttpStatus getStatus() {
 		return status;
 	}
 
-	/**
-	 * @param status the status to set
-	 */
 	public void setStatus(HttpStatus status) {
 		this.status = status;
 	}
