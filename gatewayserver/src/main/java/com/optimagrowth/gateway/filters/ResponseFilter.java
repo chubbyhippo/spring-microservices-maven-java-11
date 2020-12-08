b1b25a54aec036f99a589c68748f6bd4dfb77195
ch <mkmsacc@outlook.com>
2020-12-08 12:06:46 +0700

    Update to lombok slf4j

--- gatewayserver/src/main/java/com/optimagrowth/gateway/filters/ResponseFilter.java
+++ gatewayserver/src/main/java/com/optimagrowth/gateway/filters/ResponseFilter.java
@@ -1,19 +1,17 @@
 package com.optimagrowth.gateway.filters;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
 @Configuration
+@Slf4j
 public class ResponseFilter {
-
-	final Logger logger = LoggerFactory.getLogger(ResponseFilter.class);
 
 	@Autowired
 	FilterUtils filterUtils;
@@ -26,12 +24,12 @@ public class ResponseFilter {
 						.getHeaders();
 				String correlationId = filterUtils
 						.getCorrelationId(requestHeaders);
-				logger.debug(
+				log.debug(
 						"Adding the correlation id to the outbound headers. {}",
 						correlationId);
 				exchange.getResponse().getHeaders()
 						.add(FilterUtils.CORRELATION_ID, correlationId);
-				logger.debug("Completing outgoing request for {}.",
+				log.debug("Completing outgoing request for {}.",
 						exchange.getRequest().getURI());
 			}));
 		};
