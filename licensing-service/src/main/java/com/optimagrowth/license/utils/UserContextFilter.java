7e2205941d284733c976e8cd06a66e9be1342496
ch <mkmsacc@outlook.com>
2020-12-08 13:20:30 +0700

    Remove unused imports

--- licensing-service/src/main/java/com/optimagrowth/license/utils/UserContextFilter.java
+++ licensing-service/src/main/java/com/optimagrowth/license/utils/UserContextFilter.java
@@ -9,11 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-
 import lombok.extern.slf4j.Slf4j;
 
 @Component
