ffd2fc1a37196db9dc6607045b05c0b1fc0d7e66
ch <mkmsacc@outlook.com>
2020-12-08 12:05:48 +0700

    Update to lombok log

--- gatewayserver/src/main/java/com/optimagrowth/gateway/filters/TrackingFilter.java
+++ gatewayserver/src/main/java/com/optimagrowth/gateway/filters/TrackingFilter.java
@@ -1,7 +1,5 @@
 package com.optimagrowth.gateway.filters;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
@@ -10,14 +8,13 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
 @Order(1)
 @Component
+@Slf4j
 public class TrackingFilter implements GlobalFilter {
-
-	private static final Logger logger = LoggerFactory
-			.getLogger(TrackingFilter.class);
 
 	@Autowired
 	FilterUtils filterUtils;
@@ -27,13 +24,12 @@ public class TrackingFilter implements GlobalFilter {
 			GatewayFilterChain chain) {
 		HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
 		if (isCorrelationIdPresent(requestHeaders)) {
-			logger.debug("tmx-correlation-id found in tracking filter: {}. ",
+			log.debug("tmx-correlation-id found in tracking filter: {}. ",
 					filterUtils.getCorrelationId(requestHeaders));
 		} else {
 			String correlationID = generateCorrelationId();
 			exchange = filterUtils.setCorrelationId(exchange, correlationID);
-			logger.debug(
-					"tmx-correlation-id generated in tracking filter: {}.",
+			log.debug("tmx-correlation-id generated in tracking filter: {}.",
 					correlationID);
 		}
 
