de23150f425f2f9091b93918bf519400edc49683
ch <mkmsacc@outlook.com>
2020-12-09 22:21:26 +0700

    Refactor OrganizationRestTeplateClient to use Oauth2resttemplate

--- licensing-service/src/main/java/com/optimagrowth/license/service/client/OrganizationRestTemplateClient.java
+++ licensing-service/src/main/java/com/optimagrowth/license/service/client/OrganizationRestTemplateClient.java
@@ -3,26 +3,21 @@ package com.optimagrowth.license.service.client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
 import com.optimagrowth.license.model.Organization;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-
 @Component
 public class OrganizationRestTemplateClient {
-    @Autowired
-    RestTemplate restTemplate;
-    
-    @CircuitBreaker(name = "organizationService") 
-    public Organization getOrganization(String organizationId){
-        ResponseEntity<Organization> restExchange =
-                restTemplate.exchange(
-                        "http://organization-service/v1/organization/{organizationId}",
-                        HttpMethod.GET,
-                        null, Organization.class, organizationId);
+	@Autowired
+	OAuth2RestTemplate restTemplate;
 
-        return restExchange.getBody();
-    }
+	public Organization getOrganization(String organizationId) {
+		ResponseEntity<Organization> restExchange = restTemplate.exchange(
+				"http://localhost:8072/organization/v1/organization/{organizationId}",
+				HttpMethod.GET, null, Organization.class, organizationId);
+
+		return restExchange.getBody();
+	}
 }
