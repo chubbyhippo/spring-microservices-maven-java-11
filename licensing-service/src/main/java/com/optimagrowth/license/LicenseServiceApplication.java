919ca53b5a01220230782fb09edc0b5d9afcfe80
ch <mkmsacc@outlook.com>
2020-12-09 22:06:11 +0700

    Enable Resource Server

--- licensing-service/src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
+++ licensing-service/src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
@@ -6,13 +6,16 @@ import java.util.Locale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -23,6 +26,9 @@ import com.optimagrowth.license.utils.UserContextInterceptor;
 @RefreshScope
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableEurekaClient
+@EnableCircuitBreaker
+@EnableResourceServer
 public class LicenseServiceApplication {
 
 	public static void main(String[] args) {
@@ -35,7 +41,6 @@ public class LicenseServiceApplication {
 		localeResolver.setDefaultLocale(Locale.US);
 		return localeResolver;
 	}
-
 	@Bean
 	public ResourceBundleMessageSource messageSource() {
 		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
@@ -44,21 +49,22 @@ public class LicenseServiceApplication {
 		return messageSource;
 	}
 
-	@LoadBalanced
-	@Bean
-	public RestTemplate getRestTemplate() {
-		RestTemplate template = new RestTemplate();
-		List<ClientHttpRequestInterceptor> interceptors = template
-				.getInterceptors();
-		if (interceptors == null) {
-			template.setInterceptors(
-					Collections.singletonList(new UserContextInterceptor()));
-		} else {
-			interceptors.add(new UserContextInterceptor());
-			template.setInterceptors(interceptors);
-		}
 
-		return template;
-	}
+//	@LoadBalanced
+//	@Bean
+//	public RestTemplate getRestTemplate() {
+//		RestTemplate template = new RestTemplate();
+//		List<ClientHttpRequestInterceptor> interceptors = template
+//				.getInterceptors();
+//		if (interceptors == null) {
+//			template.setInterceptors(
+//					Collections.singletonList(new UserContextInterceptor()));
+//		} else {
+//			interceptors.add(new UserContextInterceptor());
+//			template.setInterceptors(interceptors);
+//		}
+//
+//		return template;
+//	}
 
 }
