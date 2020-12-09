91a101b85b178fd30731d2d2305fe0d1528254b3
ch <mkmsacc@outlook.com>
2020-12-09 20:41:09 +0700

    Restric delete to admin role only

--- organization-service/src/main/java/com/optimagrowth/organization/security/ResourceServerConfiguration.java
+++ organization-service/src/main/java/com/optimagrowth/organization/security/ResourceServerConfiguration.java
@@ -1,6 +1,7 @@
 package com.optimagrowth.organization.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
@@ -10,7 +11,9 @@ public class ResourceServerConfiguration
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated();
+		http.authorizeRequests()
+				.antMatchers(HttpMethod.DELETE, "v1/organization/**")
+				.hasRole("ADMIN").anyRequest().authenticated();
 	}
 
 }
