46b1f6a72a7475f0960e2113bc94b339404b9ecf
ch <mkmsacc@outlook.com>
2020-12-10 14:32:17 +0700

    Setup log level

--- organization-service/src/main/java/com/optimagrowth/organization/controller/OrganizationController.java
+++ organization-service/src/main/java/com/optimagrowth/organization/controller/OrganizationController.java
@@ -11,35 +11,44 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.optimagrowth.organization.utils.UserContextHolder;
 import com.optimagrowth.organization.model.Organization;
 import com.optimagrowth.organization.service.OrganizationService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
-@RequestMapping(value="v1/organization")
+@RequestMapping(value = "v1/organization")
+@Slf4j
 public class OrganizationController {
-    @Autowired
-    private OrganizationService service;
+	@Autowired
+	private OrganizationService service;
 
+	@RequestMapping(value = "/{organizationId}", method = RequestMethod.GET)
+	public ResponseEntity<Organization> getOrganization(
+			@PathVariable("organizationId") String organizationId) {
+		log.debug("OrganizationController Correlation id: {}",
+				UserContextHolder.getContext().getCorrelationId());
+		return ResponseEntity.ok(service.findById(organizationId));
+	}
 
-    @RequestMapping(value="/{organizationId}",method = RequestMethod.GET)
-    public ResponseEntity<Organization> getOrganization( @PathVariable("organizationId") String organizationId) {
-        return ResponseEntity.ok(service.findById(organizationId));
-    }
+	@RequestMapping(value = "/{organizationId}", method = RequestMethod.PUT)
+	public void updateOrganization(@PathVariable("organizationId") String id,
+			@RequestBody Organization organization) {
+		service.update(organization);
+	}
 
-    @RequestMapping(value="/{organizationId}",method = RequestMethod.PUT)
-    public void updateOrganization( @PathVariable("organizationId") String id, @RequestBody Organization organization) {
-        service.update(organization);
-    }
+	@PostMapping
+	public ResponseEntity<Organization> saveOrganization(
+			@RequestBody Organization organization) {
+		return ResponseEntity.ok(service.create(organization));
+	}
 
-    @PostMapping
-    public ResponseEntity<Organization>  saveOrganization(@RequestBody Organization organization) {
-    	return ResponseEntity.ok(service.create(organization));
-    }
-
-    @RequestMapping(value="/{organizationId}",method = RequestMethod.DELETE)
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteOrganization( @PathVariable("id") String id,  @RequestBody Organization organization) {
-        service.delete(organization);
-    }
+	@RequestMapping(value = "/{organizationId}", method = RequestMethod.DELETE)
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deleteOrganization(@PathVariable("id") String id,
+			@RequestBody Organization organization) {
+		service.delete(organization);
+	}
 
 }
