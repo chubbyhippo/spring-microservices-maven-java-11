10d6b06c7763501362b86bb5c7e9c90d996d844f
ch <mkmsacc@outlook.com>
2020-12-10 11:17:50 +0700

    Import ServiceConfig

--- authentication-service/src/main/java/com/optimagrowth/authentication/security/JWTTokenStoreConfig.java
+++ authentication-service/src/main/java/com/optimagrowth/authentication/security/JWTTokenStoreConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+import com.optimagrowth.authentication.config.ServiceConfig;
+
 @Configuration
 public class JWTTokenStoreConfig {
  
