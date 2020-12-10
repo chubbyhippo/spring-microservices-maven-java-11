d483b3114fae7a170a8a2c27d763679abfcabee4
ch <mkmsacc@outlook.com>
2020-12-10 09:24:19 +0700

    Replace with gateway.url env

--- licensing-service/src/main/java/com/optimagrowth/license/service/client/OrganizationRestTemplateClient.java
+++ licensing-service/src/main/java/com/optimagrowth/license/service/client/OrganizationRestTemplateClient.java
@@ -1,6 +1,7 @@
 package com.optimagrowth.license.service.client;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
@@ -11,12 +12,15 @@ import com.optimagrowth.license.model.Organization;
 @Component
 public class OrganizationRestTemplateClient {
 	@Autowired
-	OAuth2RestTemplate restTemplate;
+	private OAuth2RestTemplate restTemplate;
+
+	@Autowired
+	private Environment env;
 
 	public Organization getOrganization(String organizationId) {
 		ResponseEntity<Organization> restExchange = restTemplate.exchange(
-				"http://localhost:8072/organization/v1/organization/{organizationId}",
-				HttpMethod.GET, null, Organization.class, organizationId);
+				env.getProperty("gateway.url"), HttpMethod.GET, null,
+				Organization.class, organizationId);
 
 		return restExchange.getBody();
 	}
