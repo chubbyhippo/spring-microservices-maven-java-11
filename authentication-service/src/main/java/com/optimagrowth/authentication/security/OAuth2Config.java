240bf06c661d0ef995b6acbd8e652b170f7fcc0d
ch <mkmsacc@outlook.com>
2020-12-13 19:00:19 +0700

    Clean up imports

--- authentication-service/src/main/java/com/optimagrowth/authentication/security/OAuth2Config.java
+++ authentication-service/src/main/java/com/optimagrowth/authentication/security/OAuth2Config.java
@@ -1,7 +1,6 @@
 package com.optimagrowth.authentication.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
