3e38bfe22e26a1dbc9cb9bfcb4e792bd41d7c926
ch <mkmsacc@outlook.com>
2020-12-13 16:46:53 +0700

    Add OrganizationChangeModel POJO

--- licensing-service/src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
+++ licensing-service/src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
@@ -10,6 +10,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.support.ResourceBundleMessageSource;
@@ -21,18 +23,22 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import com.optimagrowth.license.utils.UserContextInterceptor;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
 @RefreshScope
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableEurekaClient
 @EnableResourceServer
+@EnableBinding(Sink.class)
+@Slf4j
 public class LicenseServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LicenseServiceApplication.class, args);
 	}
-
+	
 	@Bean
 	public LocaleResolver localeResolver() {
 		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
