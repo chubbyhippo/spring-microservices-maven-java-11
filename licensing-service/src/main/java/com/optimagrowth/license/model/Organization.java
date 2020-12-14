229289050eb6095452213b32362a95272c2542fa
ch <mkmsacc@outlook.com>
2020-12-14 15:56:30 +0700

    Update model

--- licensing-service/src/main/java/com/optimagrowth/license/model/Organization.java
+++ licensing-service/src/main/java/com/optimagrowth/license/model/Organization.java
@@ -1,18 +1,24 @@
 package com.optimagrowth.license.model;
 
+import org.springframework.data.redis.core.RedisHash;
 import org.springframework.hateoas.RepresentationModel;
+import javax.persistence.Id;
 
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@Getter @Setter @ToString
+@Getter
+@Setter
+@ToString
+@RedisHash("organization")
 public class Organization extends RepresentationModel<Organization> {
 
+	@Id
 	String id;
-    String name;
-    String contactName;
-    String contactEmail;
-    String contactPhone;
-    
+	String name;
+	String contactName;
+	String contactEmail;
+	String contactPhone;
+
 }
