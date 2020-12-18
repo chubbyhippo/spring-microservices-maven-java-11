514cf65799f5ac3c6f55c3b8c1ad1b75b17f53d7
ch <mkmsacc@outlook.com>
2020-12-18 18:52:20 +0700

    Remove enable feign

--- licensing-service/src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
+++ licensing-service/src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
@@ -30,7 +29,6 @@ import lombok.extern.slf4j.Slf4j;
 @SpringBootApplication
 @RefreshScope
 @EnableDiscoveryClient
-@EnableFeignClients
 @EnableEurekaClient
 @EnableResourceServer
 @EnableBinding(Sink.class)
