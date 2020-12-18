6710a8c184f67944b97b1fc35932c32fe91e97e7
ch <mkmsacc@outlook.com>
2020-12-18 18:51:48 +0700

    Remove feign dependency

--- licensing-service/src/main/java/com/optimagrowth/license/service/client/OrganizationFeignClient.java
+++ licensing-service/src/main/java/com/optimagrowth/license/service/client/OrganizationFeignClient.java
@@ -2,6 +2,7 @@ package com.optimagrowth.license.service.client;
 
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
