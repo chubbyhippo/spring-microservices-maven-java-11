b38268e325bce50683b0a08b988271a1ddb5b5ac
ch <mkmsacc@outlook.com>
2020-12-18 19:18:48 +0700

    Remove sink

--- licensing-service/src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
+++ licensing-service/src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.context.annotation.Bean;
@@ -31,7 +30,6 @@ import lombok.extern.slf4j.Slf4j;
 @EnableDiscoveryClient
 @EnableEurekaClient
 @EnableResourceServer
-@EnableBinding(Sink.class)
 @Slf4j
 public class LicenseServiceApplication {
 
