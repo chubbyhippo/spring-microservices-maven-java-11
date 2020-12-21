81f3d8df10e03dbb7c7b7098c0e61fb3a453b43b
ch <mkmsacc@outlook.com>
2020-12-21 19:57:01 +0700

    Clean up imports

--- gatewayserver/src/main/java/com/optimagrowth/gateway/filters/ResponseFilter.java
+++ gatewayserver/src/main/java/com/optimagrowth/gateway/filters/ResponseFilter.java
@@ -1,7 +1,5 @@
 package com.optimagrowth.gateway.filters;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
