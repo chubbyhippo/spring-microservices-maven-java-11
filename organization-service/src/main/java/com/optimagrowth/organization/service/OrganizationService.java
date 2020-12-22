4c6699251e801f33f3716c137d8217bfa412db99
ch <mkmsacc@outlook.com>
2020-12-22 12:53:41 +0700

    Clean up imports

--- organization-service/src/main/java/com/optimagrowth/organization/service/OrganizationService.java
+++ organization-service/src/main/java/com/optimagrowth/organization/service/OrganizationService.java
@@ -3,8 +3,6 @@ package com.optimagrowth.organization.service;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
