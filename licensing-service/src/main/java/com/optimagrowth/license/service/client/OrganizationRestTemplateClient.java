c3922216ed39656b64cff99ed03f01a9e516dde9
ch <mkmsacc@outlook.com>
2020-12-22 12:51:22 +0700

    Clean up imports

--- licensing-service/src/main/java/com/optimagrowth/license/service/client/OrganizationRestTemplateClient.java
+++ licensing-service/src/main/java/com/optimagrowth/license/service/client/OrganizationRestTemplateClient.java
@@ -1,7 +1,5 @@
 package com.optimagrowth.license.service.client;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
