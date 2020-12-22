7c67be62a4bd841693f5c9a9f928eedf6b687c1a
ch <mkmsacc@outlook.com>
2020-12-22 12:52:55 +0700

    Add custom span

--- organization-service/src/main/java/com/optimagrowth/organization/service/OrganizationService.java
+++ organization-service/src/main/java/com/optimagrowth/organization/service/OrganizationService.java
@@ -3,6 +3,8 @@ package com.optimagrowth.organization.service;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,11 +12,14 @@ import com.optimagrowth.organization.events.source.SimpleSourceBean;
 import com.optimagrowth.organization.model.Organization;
 import com.optimagrowth.organization.repository.OrganizationRepository;
 
-import lombok.extern.slf4j.Slf4j;
+import brave.ScopedSpan;
+import brave.Tracer;
 
 @Service
-@Slf4j
 public class OrganizationService {
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(OrganizationService.class);
 
 	@Autowired
 	private OrganizationRepository repository;
@@ -22,10 +27,30 @@ public class OrganizationService {
 	@Autowired
 	SimpleSourceBean simpleSourceBean;
 
+	@Autowired
+	Tracer tracer;
+
 	public Organization findById(String organizationId) {
-		Optional<Organization> opt = repository.findById(organizationId);
-		simpleSourceBean.publishOrganizationChange("GET", organizationId);
-		return (opt.isPresent()) ? opt.get() : null;
+		Optional<Organization> opt = null;
+		ScopedSpan newSpan = tracer.startScopedSpan("getOrgDBCall");
+		try {
+			opt = repository.findById(organizationId);
+			simpleSourceBean.publishOrganizationChange("GET", organizationId);
+			if (!opt.isPresent()) {
+				String message = String.format(
+						"Unable to find an organization with the Organization id %s",
+						organizationId);
+				logger.error(message);
+				throw new IllegalArgumentException(message);
+			}
+			logger.debug(
+					"Retrieving Organization Info: " + opt.get().toString());
+		} finally {
+			newSpan.tag("peer.service", "postgres");
+			newSpan.annotate("Client received");
+			newSpan.finish();
+		}
+		return opt.get();
 	}
 
 	public Organization create(Organization organization) {
@@ -53,7 +78,7 @@ public class OrganizationService {
 		try {
 			Thread.sleep(3000);
 		} catch (InterruptedException e) {
-			log.error(e.getMessage());
+			logger.error(e.getMessage());
 		}
 	}
 }
